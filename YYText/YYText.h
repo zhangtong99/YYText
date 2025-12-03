fc3be257c230d4fc5d8a0558cf037051b86f9e00
一笔一划.py <1805004786@qq.com>
2025-12-03 00:01:55 -0800

    Update YYText.h

--- YYText/YYText.h
+++ YYText/YYText.h
@@ -30,6 +30,7 @@ FOUNDATION_EXPORT const unsigned char YYTextVersionString[];
 #import <YYText/NSAttributedString+YYText.h>
 #import <YYText/NSParagraphStyle+YYText.h>
 #import <YYText/UIPasteboard+YYText.h>
+#import <YYText/YYTimer.h>
 #else
 #import "YYLabel.h"
 #import "YYTextView.h"
